14fa94f3e829f73b2eb4a7e56c80667264d759bb
Naim <mail@thenaim.com>
2021-02-20 04:20:10 +0100

    update install.sh aliases.zsh README.md

--- install.sh
+++ install.sh
@@ -1,72 +1,83 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-echo "Setting up Mac"
+is_macOS() {
+  if [ "$(uname -s)" == "Darwin" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
-# Remove .dotfiles directory if exist
-rm -rf $HOME/.dotfiles
+is_macOS ||
+  {
+    echo "[ERROR] This script can be run only on macOS"
+    exit 1
+  }
 
-# Clone Dotfiles
+echo "Setting up dotfiles....."
+
 DOTFILES=$HOME/.dotfiles
-git clone git@github.com:tankibaj/dotfiles.git $DOTFILES
-cd $DOTFILES
+ZSH=$HOME/.oh-my-zsh
+
+# Remove old files and directories if exist
+if [[ -d $DOTFILES ]]; then
+  sudo rm -rf $DOTFILES
+fi
+
+if [[ -d $ZSH ]]; then
+  sudo rm -rf $ZSH
+fi
+
+if [[ -e $HOME/.p10k.zsh ]]; then
+  sudo rm -f $HOME/.p10k.zsh
+fi
+
+if [[ -L $HOME/.zshrc ]]; then
+  sudo rm -f $HOME/.zshrc
+fi
 
 # Check for Homebrew and install if don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update Homebrew recipes
-#brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-#brew tap homebrew/bundle
-#brew bundle
-
-# Set default MySQL root password and auth type.
-#brew services restart mysql
-#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '123123'; FLUSH PRIVILEGES;"
-
-# Install PHP extensions with PECL
-#pecl install mysql json
-
-# Install global Composer packages
-#/usr/local/bin/composer global require laravel/installer laravel/valet
-
-# Install Laravel Valet
-#$HOME/.composer/vendor/bin/valet install
-
-# Create a Sites directory
-#mkdir $HOME/sites
-
-# Create a github repo directory
-#mkdir $HOME/Documents/github
+# Clone Dotfiles
+git clone git@github.com:tankibaj/dotfiles.git $DOTFILES
 
 # Backup existing .zshrc before install Oh My Zsh
 cp $HOME/.zshrc $HOME/.zshrc.original
 
-# Install Oh My Zsh
-ZSH=$HOME/.oh-my-zsh
-rm -rf $ZSH
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Clone Oh My Zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git $ZSH
 
-#### Install PowerLeve10K theme
-rm -f $HOME/.p10k.zsh
+# Clone PowerLeve10K theme
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH/themes/powerlevel10k
 
-#### Download PowerLeve10K Plugins for autosuggestion and syntax highlighting:
+# Clone PowerLeve10K plugins for autosuggestion and syntax highlighting:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm $HOME/.zshrc
+# Symlinks the .zshrc file from the .dotfiles
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# Source browser.sh
-source $HOME/.dotfiles/browser.sh
+# Update Homebrew recipes
+brew update
 
-# Load new .zshrc
-#source $HOME/.zshrc
+# Install all contents from Brewfile
+brew bundle install
 
-# Clone Github repositories
-#./clone.sh
+# # Set default MySQL root password and auth type.
+# brew services restart mysql
+# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '123123'; FLUSH PRIVILEGES;"
+
+# # Install PHP extensions with PECL
+# pecl install mysql json
+
+# # Install global Composer packages
+# /usr/local/bin/composer global require laravel/installer laravel/valet
+
+# # Install Laravel Valet
+# $HOME/.composer/vendor/bin/valet install
+
+# # Create a Sites directory
+# mkdir $HOME/Documents/Sites
