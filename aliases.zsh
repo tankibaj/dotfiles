6da8daa729529d94e16c1490188405c7f2189e63
Naim Ahmmed <mail@thenaim.com>
2022-03-21 22:03:54 +0100

    fix: remove func copyid

--- aliases.zsh
+++ aliases.zsh
@@ -84,28 +84,6 @@ alias sshconfig="vim ~/.ssh/config"
 alias sshclr='ssh-keygen -R'
 alias ssh-host-ls="grep '^Host' $HOME/.ssh/config | sed 's/Host //' | sort -u"
 
-# Copy ssh public key
-copyid() {
-  local SSH_PATH=~/.ssh/
-  IFS=';' # https://en.wikipedia.org/wiki/Input_Field_Separators
-
-  read -ra KEYS <<< "$(find ${SSH_PATH} -type f -name "*.pub" | cut -f6 -d"/" | tr '\n' ';')"
-
-  local COUNT=1
-  for i in ${KEYS[@]}; do
-    read -ra ITEMS <<< "${i}"
-    echo "  ${COUNT}) ${ITEMS[0]}" >&2
-    ((COUNT=COUNT+1))
-  done
-
-  local SELECT=0
-  while [[ ${SELECT} -lt 1 || ${SELECT} -ge $COUNT ]]; do
-    read -p "Pick a SSH Key: " SELECT >&2
-  done
-
-  cat ${SSH_PATH}/${KEYS[(SELECT-1)]} | pbcopy && echo "${KEYS[(SELECT-1)]} has been copied to clipboard"
-}
-
 # List all keys in ~/.ssh directory
 ssh-id-ls() {
   for file in ~/.ssh/*.pub; do
