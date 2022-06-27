429013c5ce7f223a037e6f5f9e2d7505b2d473c6
Naim Ahmmed <mail@thenaim.com>
2022-06-27 13:30:38 +0200

    fix: get-all and watch-all aliases

--- aliases.zsh
+++ aliases.zsh
@@ -218,8 +218,8 @@ klogs() {
 alias pod-lens='kubectl pod-lens'
 alias node-shell='kubectl node-shell'
 alias open-svc='kubectl open-svc'
-alias get-all="kubectl get all $@"
-alias watch-all="watch kubectl get all $@"
+alias get-all="kubectl get all,cm,secret,ing,pvc $@"
+alias watch-all="kubectl get all,cm,secret,ing,pvc $@"
 alias watch-pv="watch kubectl get pv,pvc $@"
 #[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
